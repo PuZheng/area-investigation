5d4784092d204ab76236bd2e342f31f85050cb49
xiechao06 <xiechao06@gmail.com>
2016-03-25 16:28:05 +0800

    fix #15

---
+++ app/src/main/java/com/puzheng/area_investigation/CreateAreaActivity.kt
@@ -0,0 +1,93 @@
+package com.puzheng.area_investigation
+
+import android.app.Dialog
+import android.databinding.DataBindingUtil
+import android.databinding.ObservableField
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentStatePagerAdapter
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.view.MenuItem
+import com.puzheng.area_investigation.databinding.ActivityCreateAreaBinding
+import kotlinx.android.synthetic.main.activity_create_area.*
+
+class CreateAreaActivity : AppCompatActivity(),
+        CreateAreaStep1Fragment.OnFragmentInteractionListener,
+        CreateAreaStep2Fragment.OnFragmentInteractionListener {
+
+    override fun afterTextChanged(s: Editable?) {
+        next.isEnabled = s.toString().isNotBlank()
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+    }
+
+    lateinit private var binding: ActivityCreateAreaBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_create_area)
+        pager.adapter = object: FragmentStatePagerAdapter(supportFragmentManager) {
+
+            private var fragments: MutableList<Fragment> = mutableListOf()
+
+            init {
+                fragments.add(CreateAreaStep1Fragment.newInstance())
+                fragments.add(CreateAreaStep2Fragment.newInstance())
+            }
+
+            override fun getItem(position: Int): Fragment? = fragments[position]
+
+            override fun getCount(): Int = 2
+        }
+        binding.args = Args(ObservableField(false), ObservableField(true))
+
+        supportActionBar?.title = getString(R.string.title_create_area)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        prev.setOnClickListener({
+            pager.currentItem -= 1
+            binding.args.hasPrevious.set(pager.currentItem > 0)
+            binding.args.hasNext.set(pager.currentItem < pager.adapter.count - 1)
+        })
+        next.setOnClickListener({
+            pager.currentItem += 1
+            binding.args.hasPrevious.set(pager.currentItem > 0)
+            binding.args.hasNext.set(pager.currentItem < pager.adapter.count - 1)
+        })
+    }
+
+    class Args(val hasPrevious: ObservableField<Boolean>, val hasNext: ObservableField<Boolean>)
+
+    override fun onBackPressed() {
+        AffirmBackDialogFragment({ super.onBackPressed() }).show(supportFragmentManager, "")
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean = when (item?.itemId) {
+        android.R.id.home -> {
+            onBackPressed()
+            true
+        }
+        else ->
+            super.onContextItemSelected(item)
+    }
+}
+
+class AffirmBackDialogFragment(val after: () -> Unit) : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(activity)
+
+        builder.setTitle(R.string.warning).setMessage(R.string.trash_cancel_create_area)
+                .setPositiveButton(R.string.confirm, {
+                    dialog, v ->
+                    after()
+                }).setNegativeButton(R.string.cancel, null)
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
---
+++ app/src/main/java/com/puzheng/area_investigation/CreateAreaStep2Fragment.kt
@@ -0,0 +1,80 @@
+package com.puzheng.area_investigation
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [CreateAreaStep2Fragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [CreateAreaStep2Fragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CreateAreaStep2Fragment : Fragment() {
+
+    // TODO: Rename and change types of parameters
+
+    private var mListener: OnFragmentInteractionListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater!!.inflate(R.layout.fragment_create_area_step2, container, false)
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    fun onButtonPressed(uri: Uri) {
+        if (mListener != null) {
+            mListener!!.onFragmentInteraction(uri)
+        }
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            mListener = context as OnFragmentInteractionListener?
+        } else {
+            throw RuntimeException(context!!.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        mListener = null
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html) for more information.
+     */
+    interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onFragmentInteraction(uri: Uri)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         * @return A new instance of fragment CreateAreaStep2Fragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        fun newInstance() = CreateAreaStep2Fragment()
+    }
+}// Required empty public constructor
