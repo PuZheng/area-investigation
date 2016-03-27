34955b20441f90b75031935cff73e52297edb86b
xiechao06 <xiechoa06@gmail.com>
2016-03-28 03:16:22 +0800

    fix #17

--- app/src/main/java/com/puzheng/area_investigation/CreateAreaActivity.kt
+++ app/src/main/java/com/puzheng/area_investigation/CreateAreaActivity.kt
@@ -4,7 +4,6 @@ import android.app.Dialog
 import android.content.Context
 import android.databinding.DataBindingUtil
 import android.databinding.ObservableField
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v4.app.Fragment
@@ -12,10 +11,13 @@ import android.support.v4.app.FragmentStatePagerAdapter
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.view.ActionMode
 import android.text.Editable
+import android.view.Menu
 import android.view.MenuItem
-import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
+import com.amap.api.maps2d.model.LatLng
+import com.orhanobut.logger.Logger
 import com.puzheng.area_investigation.databinding.ActivityCreateAreaBinding
 import kotlinx.android.synthetic.main.activity_create_area.*
 
@@ -23,20 +25,44 @@ class CreateAreaActivity : AppCompatActivity(),
         CreateAreaStep1Fragment.OnFragmentInteractionListener,
         CreateAreaStep2Fragment.OnFragmentInteractionListener {
 
+    override fun onDrawDone() {
+        drawingActionMode?.finish()
+    }
+
     override fun afterTextChanged(s: Editable?) {
         next.isEnabled = s.toString().isNotBlank()
     }
 
-    override fun onMapLongClick() {
-        // TODO into action mode
+    private var drawingActionMode: ActionMode? = null
+
+    override fun onMapLongClick(fragment: CreateAreaStep2Fragment, lnglat: LatLng) {
+        if (drawingActionMode == null) {
+            drawingActionMode = startSupportActionMode(object : ActionMode.Callback {
+                override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?) = false
+
+                override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?) = false
+
+                override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+                    fragment.startDraw()
+                    fragment.addMarker(lnglat)
+                    return true
+                }
+
+                override fun onDestroyActionMode(mode: ActionMode?) {
+                    fragment.stopDraw()
+                    drawingActionMode = null
+                }
+            })
+        }
     }
 
     lateinit private var binding: ActivityCreateAreaBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Logger.init("CreateAreaActivitiy")
         binding = DataBindingUtil.setContentView(this, R.layout.activity_create_area)
-        pager.adapter = object: FragmentStatePagerAdapter(supportFragmentManager) {
+        pager.adapter = object : FragmentStatePagerAdapter(supportFragmentManager) {
 
             private var fragments: MutableList<Fragment> = mutableListOf()
 
@@ -50,7 +76,7 @@ class CreateAreaActivity : AppCompatActivity(),
             override fun getCount(): Int = 2
 
         }
-        pager.addOnPageChangeListener(object: ViewPager.SimpleOnPageChangeListener() {
+        pager.addOnPageChangeListener(object : ViewPager.SimpleOnPageChangeListener() {
             override fun onPageSelected(position: Int) {
                 val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                 // 填写名称时要求展示输入法, 勾勒轮廓时隐藏输入法
@@ -96,7 +122,7 @@ class CreateAreaActivity : AppCompatActivity(),
     }
 }
 
-class AffirmBackDialogFragment(val after: () -> Unit) : DialogFragment() {
+private class AffirmBackDialogFragment(val after: () -> Unit) : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val builder = AlertDialog.Builder(activity)
 
@@ -109,3 +135,18 @@ class AffirmBackDialogFragment(val after: () -> Unit) : DialogFragment() {
         return builder.create();
     }
 }
+
+private class CancelDrawingDialogFragment(val after: () -> Unit) : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(activity)
+
+        builder.setTitle(R.string.warning).setMessage(R.string.cancel_drawing)
+                .setPositiveButton(R.string.confirm, {
+                    dialog, v ->
+                    after()
+                }).setNegativeButton(R.string.cancel, null)
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
+
--- app/src/main/java/com/puzheng/area_investigation/CreateAreaStep2Fragment.kt
+++ app/src/main/java/com/puzheng/area_investigation/CreateAreaStep2Fragment.kt
@@ -3,24 +3,27 @@ package com.puzheng.area_investigation
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
-import android.net.Uri
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Point
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
+import android.support.v4.view.MotionEventCompat
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.amap.api.location.AMapLocationClient
 import com.amap.api.location.AMapLocationClientOption
-import com.amap.api.maps2d.AMap
 import com.amap.api.maps2d.CameraUpdateFactory
 import com.amap.api.maps2d.LocationSource
-import com.amap.api.maps2d.model.CameraPosition
+import com.amap.api.maps2d.model.*
 import com.orhanobut.logger.Logger
 import kotlinx.android.synthetic.main.fragment_create_area_step2.*
-
 
 /**
  * A simple [Fragment] subclass.
@@ -36,24 +39,63 @@ class CreateAreaStep2Fragment : Fragment() {
 
     private var listener: OnFragmentInteractionListener? = null
 
+    private val closeToLimit: Int by lazy {
+        val metrics = resources.displayMetrics
+        32 * (metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT)
+    }
+
+    private val horizontalBoundaryLimit: Int by lazy {
+        val metrics = resources.displayMetrics
+        40 * (metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT)
+    }
+
+    private val verticalBoundaryLimit: Int by lazy {
+        val metrics = resources.displayMetrics
+        40 * (metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT)
+    }
+
+    // 地图清除所有覆盖物时，会回收所有关联的Bitmap
+    private val touchingMarkerBitmap: Bitmap? = null
+        get() = if (field == null || field.isRecycled) makeBitmap(R.drawable.touching_marker) else field
+
+    private val closingMarkerBitmap: Bitmap? = null
+        get() = if (field == null || field.isRecycled) makeBitmap(R.drawable.closing_marker) else field
+
+    private val markerBitmap: Bitmap? = null
+        get() = if (field == null || field.isRecycled) makeBitmap(R.drawable.marker) else field
+
+    private fun makeBitmap(resId: Int): Bitmap {
+        val canvas = Canvas()
+        val drawable = ContextCompat.getDrawable(activity, resId)
+        val bitmap = Bitmap.createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight,
+                Bitmap.Config.ARGB_8888)
+        canvas.setBitmap(bitmap)
+        drawable.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
+        drawable.draw(canvas)
+        return bitmap
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        val view = inflater!!.inflate(R.layout.fragment_create_area_step2, container, false)
-        return view;
-    }
+                              savedInstanceState: Bundle?) = inflater!!.inflate(R.layout.fragment_create_area_step2, container, false)
 
 
     private var onLocationChangeListener: LocationSource.OnLocationChangedListener? = null
 
+    private val isAreaClosed: Boolean
+        get() = markers.size > 2 && markers.first().screenLocation == markers.last().screenLocation
+
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+
         map.onCreate(savedInstanceState)
-        map.map.setLocationSource(object: LocationSource {
+        map.map.setLocationSource(object : LocationSource {
             override fun deactivate() {
                 onLocationChangeListener = null
             }
@@ -67,12 +109,49 @@ class CreateAreaStep2Fragment : Fragment() {
         map.map.isMyLocationEnabled = true
         map.map.moveCamera(CameraUpdateFactory.zoomTo(INIT_ZOOM_LEVEL))
         map.map.setOnMapLongClickListener {
-            listener?.onMapLongClick()
+            listener?.onMapLongClick(this, it)
+        }
+        var lastScreenLocation: Point? = null
+        map.map.setOnMapTouchListener {
+            if (!map.map.uiSettings.isScrollGesturesEnabled) {
+                when (MotionEventCompat.getActionMasked(it)) {
+                    MotionEvent.ACTION_UP -> {
+                        activeMarker?.setIcon(BitmapDescriptorFactory.fromBitmap(markerBitmap))
+                        activePolyline.color = ContextCompat.getColor(activity, R.color.colorAccent)
+                        startMarker?.setIcon(BitmapDescriptorFactory.fromBitmap(markerBitmap))
+                        if (isAreaClosed) {
+                            Toast.makeText(activity, "区域勾勒成功", Toast.LENGTH_SHORT).show()
+                            stopDraw()
+                            listener?.onDrawDone()
+                        }
+                    }
+                    MotionEvent.ACTION_DOWN -> {
+                        addMarker(it.latlng)
+                        lastScreenLocation = it.screenLocation
+                    }
+                    MotionEvent.ACTION_MOVE -> {
+                        activeMarker?.position = it.latlng
+                        // 一定要构成多边形
+                        if (markers.size > 2) {
+                            if (isCloseToStartMarker(it.screenLocation, lastScreenLocation)) {
+                                startMarker?.setIcon(BitmapDescriptorFactory.fromBitmap(closingMarkerBitmap))
+                                // 自动吸附到出发点
+                                activeMarker?.position = startMarker!!.position
+                            } else {
+                                startMarker?.setIcon(BitmapDescriptorFactory.fromBitmap(markerBitmap))
+                            }
+                        }
+                        val (scrollX, scrollY) = violationToBoundary(it.screenLocation)
+                        Logger.v("$scrollX $scrollY")
+                        map.map.moveCamera(CameraUpdateFactory.scrollBy(scrollX, scrollY))
+                        activePolyline?.points = listOf(activeMarker?.position, markers[markers.lastIndex - 1].position)
+                        lastScreenLocation = it.screenLocation
+                    }
+                }
+            }
         }
 
-
-//        val permissionCheck = ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION)
-        val permissionCheck = ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION)
+        val permissionCheck = ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION)
         if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
             startLocation()
         } else {
@@ -88,8 +167,36 @@ class CreateAreaStep2Fragment : Fragment() {
                         PERSISSION_TO_ACCESS_FINE_LOCATION)
             }
         }
+    }
 
 
+
+    private fun violationToBoundary(screenLocation: Point): FloatArray {
+        val scrollX = if (screenLocation.x < horizontalBoundaryLimit) {
+            screenLocation.x - horizontalBoundaryLimit.toFloat()
+        } else if (screenLocation.x + horizontalBoundaryLimit > map.width) {
+            screenLocation.x + horizontalBoundaryLimit.toFloat() - map.width
+        } else {
+            0.0F
+        }
+        val scrollY = if (screenLocation.y < verticalBoundaryLimit) {
+            screenLocation.y - verticalBoundaryLimit.toFloat()
+        } else if (screenLocation.y + verticalBoundaryLimit > map.height) {
+            screenLocation.y + verticalBoundaryLimit.toFloat() - map.height
+        } else {
+            0.0F
+        }
+        return floatArrayOf(scrollX, scrollY)
+    }
+
+    private fun isCloseToStartMarker(screenLocation: Point, lastScreenLocation: Point?): Boolean {
+        if (startMarker == null) {
+            return false
+        }
+        val isApproaching = lastScreenLocation == null || (
+                lastScreenLocation!!.distanceTo(startMarker!!.screenLocation) >
+                        screenLocation.distanceTo(startMarker!!.screenLocation))
+        return isApproaching && startMarker!!.screenLocation.distanceTo(screenLocation) < closeToLimit
     }
 
 
@@ -150,7 +257,9 @@ class CreateAreaStep2Fragment : Fragment() {
      */
     interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
-        fun onMapLongClick()
+        fun onMapLongClick(fragment: CreateAreaStep2Fragment, lnglat: LatLng)
+
+        fun onDrawDone()
     }
 
     companion object {
@@ -161,8 +270,58 @@ class CreateAreaStep2Fragment : Fragment() {
          */
         // TODO: Rename and change types and number of parameters
         fun newInstance() = CreateAreaStep2Fragment()
+
         private val PERSISSION_TO_ACCESS_FINE_LOCATION: Int = 100
         private val INIT_ZOOM_LEVEL: Float = 16.0F
 
     }
+
+    fun startDraw() {
+        map.map.uiSettings.isScrollGesturesEnabled = false
+        map.map.uiSettings.isZoomGesturesEnabled = false
+    }
+
+    fun stopDraw() {
+        map.map.uiSettings.isScrollGesturesEnabled = true
+        map.map.uiSettings.isZoomGesturesEnabled = true
+        markers.clear()
+        map.map.clear()
+    }
+
+    private var activeMarker: Marker? = null
+        get() = markers.last()
+
+    private var startMarker: Marker? = null
+        get() = markers.first()
+
+    private val markers = mutableListOf<Marker>()
+
+    lateinit private var activePolyline: Polyline
+
+    fun addMarker(latlng: LatLng) {
+        markers.add(map.map.addMarker(
+                MarkerOptions().icon(BitmapDescriptorFactory.fromBitmap(touchingMarkerBitmap))
+                        .position(latlng).anchor(0.5F, 0.5F)))
+        if (markers.size > 1) {
+            activePolyline = map.map.addPolyline(
+                    PolylineOptions().add(activeMarker?.position, markers[markers.lastIndex - 1].position)
+                            .width(4.0F).color(ContextCompat.getColor(activity, R.color.colorAccentLighter)))
+        } else {
+            activeMarker?.zIndex = 1000.0F // 出发点要在所有点的上面
+        }
+    }
+
+    // MR extensions
+    private val Marker.screenLocation: Point
+        get() = map.map.projection.toScreenLocation(position)
+
+    private val MotionEvent.screenLocation: Point
+        get() = Point(x.toInt(), y.toInt())
+
+    private val MotionEvent.latlng: LatLng
+        get() = map.map.projection.fromScreenLocation(screenLocation)
+
+    private fun Point.distanceTo(point: Point) = Math.sqrt(
+            (Math.pow((point.x - x).toDouble(), 2.0) + Math.pow((point.y - y).toDouble(), 2.0)))
 }
+
