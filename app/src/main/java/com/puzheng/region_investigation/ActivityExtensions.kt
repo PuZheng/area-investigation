31fe6fcf5a4b2c77a6f968697883885b4058d384
xiechao06 <xiechao06@gmail.com>
2016-05-12 18:06:38 +0800

    fix #129

--- app/src/main/java/com/puzheng/region_investigation/ActivityExtensions.kt
+++ app/src/main/java/com/puzheng/region_investigation/ActivityExtensions.kt
@@ -1,16 +1,22 @@
 package com.puzheng.region_investigation
 
 import android.app.Activity
-import android.app.AlertDialog
 import android.app.Dialog
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
+import android.net.ConnectivityManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.app.AppCompatDialogFragment
 import android.view.View
@@ -58,3 +64,30 @@ fun AppCompatActivity.assertPermission(permission: String, requestCode: Int, rat
 fun <T : Fragment?> AppCompatActivity.findFragmentById(id: Int) = supportFragmentManager.findFragmentById(id) as T
 
 fun <T : View?> Activity.findView(id: Int) = findViewById(id) as T
+
+fun AppCompatActivity.assertNetwork() = promiseOnUi {
+    val manager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val info = manager.activeNetworkInfo
+
+    if (info == null || !info.isAvailable) {
+
+        object: AppCompatDialogFragment() {
+            override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+                return AlertDialog.Builder(this@assertNetwork).setTitle(R.string.warning).setMessage("网络不可访问，请前去设置")
+                        .setPositiveButton(R.string.confirm, {
+                           dialog, which ->
+                            startActivity(
+                                    if (Build.VERSION.SDK_INT > 10) {
+                                        Intent(Settings.ACTION_WIRELESS_SETTINGS);
+                                    } else {
+                                        Intent().apply {
+                                            intent.component = ComponentName("com.android.settings", "com.android.settings.WirelessSettings");
+                                            intent.action = "android.intent.action.VIEW";
+                                        }
+                                    })
+                        }).setNegativeButton(R.string.cancel, null).show()
+            }
+        }.show(supportFragmentManager, "");
+        throw Exception("network unavailable")
+    }
+}
